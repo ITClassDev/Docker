5aef2e949e176c3959339415137b8575a3a0ce3a
Slava Petrov <slavapmk@gmail.com>
2023-04-14 14:12:33 +0300

    Added arch install script

---
+++ install-arch.sh
@@ -0,0 +1,6 @@
+cd ..
+sudo pacman -S docker docker-compose
+sudo systemctl enable docker
+sudo systemctl start docker
+git clone https://github.com/ITClassDev/Backend
+git clone https://github.com/ITClassDev/FrontEnd
