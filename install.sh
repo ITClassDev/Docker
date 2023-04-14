b45f9058e678747f31c0f8846af530373c329acd
Slava Petrov <slavapmk@gmail.com>
2023-04-14 14:11:08 +0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+cd ..
+sudo apt install docker docker-compose -fy
+sudo systemctl enable docker
+sudo systemctl start docker
+git clone https://github.com/ITClassDev/Backend
+git clone https://github.com/ITClassDev/FrontEnd
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # Simple script to run docker-compose
 source ./.env
-docker-compose -f docker-compose.dev.yaml up
+docker-compose -f docker-compose.dev.yaml up --build
