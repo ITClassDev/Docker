0892177b4a10268a77af1900edd61bcce59c9bcd
ret7020 <ret7020@gmail.com>
2023-08-27 19:55:53 +0300

    Lazy install script

---
+++ lazy_install.sh
@@ -0,0 +1,16 @@
+# Setup docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Download sources
+mkdir ShTP && cd ShTP
+git clone https://github.com/ItClassDev/Docker # Docker configs
+git clone https://github.com/ItClassDev/Frontend # Frontend sources
+git clone https://github.com/ItClassDev/Backend # Backend sources
+
+# Run IT
+cd Docker
+./run.sh
